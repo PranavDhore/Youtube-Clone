cbcc9bbd5f79fcc05e6bd74ed31d894e419b2fb4
= <=>
2023-06-15 13:47:10 +0530

    Configured environment variables

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 const VideoCard = ({video}) => {
     console.log(video)
   return (
-    <div className='flex xl:mx-4 flex-wrap justify-center pt-7 mx-3'>
+    <div className='flex gap-2 xl:mx-4 flex-wrap justify-center pt-7 mx-3'>
       {video.map((item,index)=>{
         const title = item.video.title;
-        const thumbnail = item.video.thumbnails[0]['url'];
-        // console.log(thumbnail)
+        const thumbnail = item.video.thumbnails[0];
+        const movingThumbnail = item.video['movingThumbnails'];
+        console.log(movingThumbnail)
         return (
-            <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 max-w-sm rounded-lg'>
+            <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 max-w-sm max-h-xs rounded-lg'>
                 <a href='#'>
-                    <img className='rounded-t-lg' src={thumbnail} />
+                    <img className={`rounded-t-lg`} width={thumbnail.width} height={thumbnail.height} src={thumbnail['url']} />
                 </a>
                 <div>
                     <p className='mb-3 font-normal'>{title}</p>
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,17 +4,17 @@ export const fetchYoutubeData = async (url) =>{
 
     const options = {
         method: 'GET',
-        url: `https://youtube138.p.rapidapi.com/${url}/`,
+        url: `${process.env.REACT_APP_YOUTUBE_API_URL}${url}/`,
         params: {hl: 'en', gl: 'US'},
         headers: {
-          'X-RapidAPI-Key': '426e511a0cmshc3d8a7be642496fp1e16e3jsn04ab4f9d2605',
-          'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+          'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API,
+          'X-RapidAPI-Host': process.env.REACT_APP_YOUTUBE_HOST
         }
     };
 
     try {
         const response = await axios.request(options);
-        // console.log(response.data);
+        
         return response.data;
     } catch (error) {
         console.error(error);
